320b7dfe2cd9865b3e24d3b64198212aa66fb149
Lance <111308100+LanceScheffer@users.noreply.github.com>
2023-01-12 03:03:00 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+# p Method
+string = "p"
+p string 
+
+# puts Method
+string = "puts"
+puts string
+
+# print Method
+string = "print"
+print string
+
+# 
+my_name = "Lance"
+puts my_name
+
+# 
+beginning_sentence = "My name is "
+full_sentence = beginning_sentence + my_name
+puts full_sentence
